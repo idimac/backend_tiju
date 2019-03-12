f335b9c091fb92fef4deff5c6233dd8305769df2
Dmitry Shlyahov <shlyahovdmitry@gmail.com>
2019-03-12 21:51:36 +0200

    add morgan middleware to simplify output in console

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import session from 'express-session';
 import bodyParser from 'body-parser';
 import configureRouter from './controllers';
+import morgan from 'morgan';
 
 import config from './config';
 
@@ -17,6 +18,7 @@ app.listen(config.port, (err) => {
 
 
 app.use(bodyParser.json());         //this middleware handles POST request bodies
+app.use(morgan('tiny'));// combined // to simplify output in console
 app.use(session({                   //this middleware saves sessions information on server and coockies
     resave: true,
     saveUninitialized: true,
